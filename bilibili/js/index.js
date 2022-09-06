d788912de58d96ef93c78542f17d6c1c6f324e1d
blueDange <110520809+blueDange@users.noreply.github.com>
2022-09-06 09:03:03 +0800

    Add files via upload

---
+++ bilibili/js/index.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: blueDange 672074694@qq.com
+ * @Date: 2022-09-04 19:29:41
+ * @LastEditors: blueDange 672074694@qq.com
+ * @LastEditTime: 2022-09-05 14:22:27
+ * @FilePath: \bilibili\js\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+window.addEventListener('load', function() {
+    
+    ipt.addEventListener('click', function() {
+    let ss = document.querySelector('.ss')
+    let ssa = ss.querySelector('.ssa');
+    let ipt = document.querySelector('#ipt'); 
+    let hot_search  = document.querySelector('.hot_search ')
+        ss.style.backgroundColor = '#fff';
+        ipt.style.backgroundColor = '#E3E5E7'
+        hot_search.style.display = 'flex';
+        ssa.style.display = 'flex';
+        alert('nihao')
+    })
+    ssa.addEventListener('blur', function() {
+        ssa.style.display = 'none';
+        hot_search .style.display = 'none'
+    })
+    
+})
