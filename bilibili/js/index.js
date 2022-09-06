e9cb5ddc33135bb8bebc517f566d2e55b143c41f
blueDange <672074694@qq.com>
2022-09-06 20:11:57 +0800

    修改样式

--- bilibili/js/index.js
+++ bilibili/js/index.js
@@ -2,7 +2,7 @@
  * @Author: blueDange 672074694@qq.com
  * @Date: 2022-09-04 19:29:41
  * @LastEditors: blueDange 672074694@qq.com
- * @LastEditTime: 2022-09-05 14:22:27
+ * @LastEditTime: 2022-09-06 19:55:08
  * @FilePath: \bilibili\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,14 +14,11 @@ window.addEventListener('load', function() {
     let ipt = document.querySelector('#ipt'); 
     let hot_search  = document.querySelector('.hot_search ')
         ss.style.backgroundColor = '#fff';
-        ipt.style.backgroundColor = '#E3E5E7'
-        hot_search.style.display = 'flex';
-        ssa.style.display = 'flex';
-        alert('nihao')
+        // ssa.style.backgroundColor = '#fff'
+        // ipt.style.backgroundColor = '#E3E5E7'
+        hot_search.style.display = 'block';
+        ssa.style.display = 'block';
     })
-    ssa.addEventListener('blur', function() {
-        ssa.style.display = 'none';
-        hot_search .style.display = 'none'
-    })
+   
     
 })
