af514137764cf915cdaae222bfb7aa27f4eabccb
blueDange <672074694@qq.com>
2022-09-08 20:04:40 +0800

    右边响应

--- bilibili/js/index.js
+++ bilibili/js/index.js
@@ -2,23 +2,27 @@
  * @Author: blueDange 672074694@qq.com
  * @Date: 2022-09-04 19:29:41
  * @LastEditors: blueDange 672074694@qq.com
- * @LastEditTime: 2022-09-06 19:55:08
+ * @LastEditTime: 2022-09-08 09:16:34
  * @FilePath: \bilibili\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 window.addEventListener('load', function() {
-    
-    ipt.addEventListener('click', function() {
     let ss = document.querySelector('.ss')
     let ssa = ss.querySelector('.ssa');
     let ipt = document.querySelector('#ipt'); 
     let hot_search  = document.querySelector('.hot_search ')
-        ss.style.backgroundColor = '#fff';
-        // ssa.style.backgroundColor = '#fff'
-        // ipt.style.backgroundColor = '#E3E5E7'
-        hot_search.style.display = 'block';
-        ssa.style.display = 'block';
-    })
-   
+//     ipt.addEventListener('click', function() {
+
+//         ss.style.backgroundColor = '#fff';
+//         // ssa.style.backgroundColor = '#fff'
+//         // ipt.style.backgroundColor = '#E3E5E7'
+//         hot_search.style.display = 'block';
+//         ssa.style.display = 'block';
+//     })
+//    ipt.addEventListener('blur', function() {
+//     hot_search.style.display = 'none';
+//         ssa.style.display = 'none';
+//         ss.style.backgroundColor = 'rgba(225,225,225,0)'
+//    })
     
 })
