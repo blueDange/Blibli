49def56bbdd09b20a34564619ded9741a087355e
blueDange <672074694@qq.com>
2022-10-30 21:37:47 +0800

    banner

--- bilibili/js/index.js
+++ bilibili/js/index.js
@@ -12,7 +12,31 @@ window.addEventListener("load", function () {
 
   });
   // 轮播图
+  // 点击小圆点滚动图片
+  const $banner = $('.banner')
+  const $banner_width = $banner.width()
+  console.log($banner_width)
 
+  // 小圆点
+  // 动态生成小圆点
+  const $ul = $('.banner ul').children()
+  const $ol = $('.banner .circle')
+  $ul.each(function (index, element) {
+
+    let li = document.createElement('li')
+    $ol.append(li)
+    $ol.children().first().addClass('current')
+
+  })
+
+
+  var $img = $('.banner>ul>li')
+  $img.eq(0).show().siblings().hide()
+  $('.circle li').on('click', function () {
+    $(this).addClass('current').siblings().removeClass('current')
+    const i = $(this).index()
+    $img.eq(i).show().siblings().hide()
+  })
 
   $("#btn>button").on("click", function () {
     $(this).addClass("blue").siblings().removeClass("blue");
